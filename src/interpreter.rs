864bdaa9700271290a0878ff41571ad1099c0124
= <=>
2021-01-28 09:23:25 +0900

    interpreter area bug fixed

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -7,21 +7,21 @@ type Output = Result<()>;
 
 pub struct Interpreter {
     area: ProgramArea,
-    now: usize,
+    pointer: usize,
 }
 
 impl Interpreter {
     pub fn new() -> Self {
         Self {
             area: [0; AREA_SIZE],
-            now: 0
+            pointer: 0
         }
     }
 
     pub fn from_ast(&mut self, ast: Vec<Node>) -> Output {
         let eval = Eval::new();
         for node in ast {
-            eval.eval(&node, &mut self.area, &mut self.now);
+            eval.eval(&node, &mut self.area, &mut self.pointer);
         }
         Ok(())
     }
@@ -39,27 +39,27 @@ struct Eval;
 impl Eval {
     pub fn new() -> Self { Self }
 
-    pub fn eval(&self, node: &Node, area: &mut [u8], now: &mut usize) {
+    pub fn eval(&self, node: &Node, area: &mut [u8], pointer: &mut usize) {
         match node {
             Node::Operation { op } => {
                 match op {
                     Operation::PInc => {
-                        *now += 1;
-                        if *now == AREA_SIZE { *now = 0 }
+                        *pointer += 1;
+                        if *pointer == AREA_SIZE { *pointer = 0 }
                     },
                     Operation::PDec => {
-                        if *now == 0 { *now = AREA_SIZE }
-                        *now -= 1;
+                        if *pointer == 0 { *pointer = AREA_SIZE }
+                        *pointer -= 1;
                     },
                     Operation::VInc => {
-                        if area[*now] == u8::MAX - 1 { area[*now] = 0 }
-                        else { area[*now] += 1 }
+                        if area[*pointer] == u8::MAX { area[*pointer] = 0 }
+                        else { area[*pointer] += 1 }
                     },
                     Operation::VDec => {
-                        if area[*now] == 0 { area[*now] = u8::MAX - 1 }
-                        else { area[*now] -= 1 }
+                        if area[*pointer] == 0 { area[*pointer] = u8::MAX }
+                        else { area[*pointer] -= 1 }
                     },
-                    Operation::Output => print!("{}", area[*now] as char),
+                    Operation::Output => print!("{}", area[*pointer] as char),
                     Operation::Input => {
                         let input = io::stdin()
                             .bytes()
@@ -67,14 +67,14 @@ impl Eval {
                             .and_then(|r| r.ok())
                             .map(|b| b as u8)
                             .unwrap();
-                        area[*now] = input;
+                        area[*pointer] = input;
                     },
                 }
             },
             Node::Repetition { child } => {
-                while area[*now] != 0 {
+                while area[*pointer] != 0 {
                     for node in child.iter() {
-                        self.eval(node, area, now);
+                        self.eval(node, area, pointer);
                     }
                 }
             },
